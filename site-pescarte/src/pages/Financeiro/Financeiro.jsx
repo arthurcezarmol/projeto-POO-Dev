0dac76a2dc03b8e1198a34328ab87d19eca81559
Arthur Mól <arthurcezarmol@gmail.com>
2025-10-06 09:50:54 -0300

    Revert "Atualização do relatório (semanas 2 e 3) e alteração na estrutura das pastas"
    This reverts commit 79657d1cddbdb5d315aeabb15cae15e04b2ff7e2.

---
+++ site-pescarte/src/pages/Financeiro/Financeiro.jsx
@@ -0,0 +1,10 @@
+function Financeiro () {
+    return (
+        <>
+            <h1>Financeiro</h1>
+            <p>Aqui irão as funcionalidades para gerar planilhas.</p>
+        </>
+    );
+}
+
+export default Financeiro;
